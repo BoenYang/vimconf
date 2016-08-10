a63babeaaacb84669280f16385e2d405d4c29d63
root <root@localhost.localdomain>
2016-08-10 16:52:01 +0000

    1.add arch install script 2.modify vim rc

---
+++ install_arch.sh
@@ -0,0 +1,8 @@
+#git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+CONFIG_FILE="/vimrc"
+SHELL_PATH=$(cd "$(dirname "$0")";pwd)
+CONFIG_PATH=${SHELL_PATH}${CONFIG_FILE}
+echo $CONFIG_PATH
+
+rm -vf ~/.vimrc
+ln -s $CONFIG_PATH ~/.vimrc
